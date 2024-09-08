b2a1ba8f11f37525ace07575df143bff02b67acf
Christopher Ralph <chris.734c@gmail.com>
2024-09-07 21:55:25 -0400

    📦 NEW: Implemented the restricted middleware to verify JWTs.

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,6 @@
 const { JWT_SECRET } = require('../secrets'); // use this secret!
 const { find, findBy } = require('../users/users-model');
+const jwt = require('jsonwebtoken');
 const restricted = (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
@@ -16,6 +17,18 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
+  const token = req.headers.authorization;
+  if (!token) {
+    return next({ status: 401, message: 'Token required' });
+  }
+  jwt.verify(token, JWT_SECRET, (error, decodedToken) => {
+    if (error) {
+      next({ status: 401, message: 'Token invalid' });
+    } else {
+      req.decodedToken = decodedToken;
+      next();
+    }
+  });
 };
 
 const only = (role_name) => (req, res, next) => {
