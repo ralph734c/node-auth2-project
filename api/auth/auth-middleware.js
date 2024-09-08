0b65fa808962cee2b1358342103cab1db0c23471
Christopher Ralph <chris.734c@gmail.com>
2024-09-07 21:43:10 -0400

    📦 NEW: Completed login auth endpoint with jwt and checkUsernameExists middleware

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,5 @@
 const { JWT_SECRET } = require('../secrets'); // use this secret!
-
+const { find, findBy } = require('../users/users-model');
 const restricted = (req, res, next) => {
   /*
     If the user does not provide a token in the Authorization header:
@@ -16,7 +16,6 @@ const restricted = (req, res, next) => {
 
     Put the decoded token in the req object, to make life easier for middlewares downstream!
   */
-  next();
 };
 
 const only = (role_name) => (req, res, next) => {
@@ -33,7 +32,7 @@ const only = (role_name) => (req, res, next) => {
   next();
 };
 
-const checkUsernameExists = (req, res, next) => {
+const checkUsernameExists = async (req, res, next) => {
   /*
     If the username in req.body does NOT exist in the database
     status 401
@@ -41,7 +40,17 @@ const checkUsernameExists = (req, res, next) => {
       "message": "Invalid credentials"
     }
   */
-  next();
+  try {
+    const [user] = await findBy({ username: req.body.username });
+    if (!user) {
+      next({ status: 401, message: 'Invalid credentials' });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 const validateRoleName = (req, res, next) => {
