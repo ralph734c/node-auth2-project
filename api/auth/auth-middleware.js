f4cba1c93e1d5a44e6a240ff9f0da2c1da9cdda6
Christopher Ralph <chris.734c@gmail.com>
2024-09-07 22:08:33 -0400

    Removed unnecessary comments

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,22 +1,8 @@
-const { JWT_SECRET } = require('../secrets'); // use this secret!
+const { JWT_SECRET } = require('../secrets');
 const { findBy } = require('../users/users-model');
 const jwt = require('jsonwebtoken');
+
 const restricted = (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header:
-    status 401
-    {
-      "message": "Token required"
-    }
-
-    If the provided token does not verify:
-    status 401
-    {
-      "message": "Token invalid"
-    }
-
-    Put the decoded token in the req object, to make life easier for middlewares downstream!
-  */
   const token = req.headers.authorization;
   if (!token) {
     return next({ status: 401, message: 'Token required' });
@@ -32,16 +18,6 @@ const restricted = (req, res, next) => {
 };
 
 const only = (role_name) => (req, res, next) => {
-  /*
-    If the user does not provide a token in the Authorization header with a role_name
-    inside its payload matching the role_name passed to this function as its argument:
-    status 403
-    {
-      "message": "This is not for you"
-    }
-
-    Pull the decoded token from the req object, to avoid verifying it again!
-  */
   if (role_name === req.decodedToken.role_name) {
     next();
   } else {
@@ -50,13 +26,6 @@ const only = (role_name) => (req, res, next) => {
 };
 
 const checkUsernameExists = async (req, res, next) => {
-  /*
-    If the username in req.body does NOT exist in the database
-    status 401
-    {
-      "message": "Invalid credentials"
-    }
-  */
   try {
     const [user] = await findBy({ username: req.body.username });
     if (!user) {
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require('../secrets'); // use this secret!
+const { JWT_SECRET } = require('../secrets');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../users/users-model');
@@ -17,25 +17,6 @@ router.post('/register', validateRoleName, (req, res, next) => {
 });
 
 router.post('/login', checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
   if (bcrypt.compareSync(req.body.password, req.user.password)) {
     const token = buildToken(req.user);
     res.json({
