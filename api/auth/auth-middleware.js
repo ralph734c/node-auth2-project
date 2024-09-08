cd23136545b72800763477c75c1d5ba8dd1e63a3
Christopher Ralph <chris.734c@gmail.com>
2024-09-07 20:41:01 -0400

    📦 NEW: Completed the user registration endpoint

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -45,24 +45,6 @@ const checkUsernameExists = (req, res, next) => {
 };
 
 const validateRoleName = (req, res, next) => {
-  /*
-    If the role_name in the body is valid, set req.role_name to be the trimmed string and proceed.
-
-    If role_name is missing from req.body, or if after trimming it is just an empty string,
-    set req.role_name to be 'student' and allow the request to proceed.
-
-    If role_name is 'admin' after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be admin"
-    }
-
-    If role_name is over 32 characters after trimming the string:
-    status 422
-    {
-      "message": "Role name can not be longer than 32 chars"
-    }
-  */
   if (!req.body.role_name || !req.body.role_name.trim()) {
     req.role_name = 'student';
     next();
@@ -71,7 +53,8 @@ const validateRoleName = (req, res, next) => {
   } else if (req.body.role_name.trim().length > 32) {
     next({ status: 422, message: 'Role name can not be longer than 32 chars' });
   } else {
-    next()
+    req.role_name = req.body.role_name.trim();
+    next();
   }
 };
 
